693db674edacb05d9e2cc8a2ab70c2e2d6a4abdc
alicj <alicjakowalska0624@gmail.com>
2020-05-10 21:48:01 +0200

    Add new files

---
+++ src/main/java/wizut/tpsi/lab9/BlogController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wizut.tpsi.lab9;
+
+import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author alicj
+ */
+@Controller
+public class BlogController {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @RequestMapping("/")
+    public String index(Model model) throws SQLException {
+        model.addAttribute("blogRepository", blogRepository.getAllPosts());
+        return "index";
+    }
+    
+    @PostMapping("/post")
+    public String post(Model model,BlogPost post) throws SQLException {
+        blogRepository.deletePost(post);
+        model.addAttribute("blogRepository", blogRepository.getAllPosts());
+        return "redirect:/";
+    }
+
+    @PostMapping("/newpost")
+    public String newPost(Model model,BlogPost post) throws SQLException {
+        blogRepository.createPost(post);
+        model.addAttribute("blogRepository", blogRepository.getAllPosts());
+        return "redirect:/";
+    }
+}
